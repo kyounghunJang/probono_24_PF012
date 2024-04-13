9ad5a0fe91b2ba8363e7631b3e451265c6c72b46
kyounghoonJang <matkimchi_@naver.com>
2024-04-14 02:04:58 +0900

    init

--- terraform/ variables.tf
+++ terraform/ variables.tf
@@ -7,3 +7,9 @@ variable "region" {
   description = "The region to deploy to"
   type        = string
 }
+
+variable "gke_num_nodes" {
+  description = "The number of nodes in the GKE cluster"
+  type        = number
+  default     = 3
+}
